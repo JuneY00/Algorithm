a7ce2743359d84dfd1b3a0223bdb913347f3948e
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-07 17:31:14 +0900

    Time: 8 ms (9.94%), Space: 82.1 MB (47.19%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -10,28 +10,30 @@
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        
-        if(head == null){
-            return head;
+        // No need to process if the list is empty or only contains a single node
+        if (head == null || head.next == null) {
+            return null; 
         }
-        
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int num = 0;
-        
-        while(head !=null){
 
-            if(head.val == 0){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy; // Pointer to track the new list being constructed
+        int num = 0; // To accumulate values between zeros
+
+        head = head.next; // Skip the initial zero
+
+        while (head != null) {
+            if (head.val == 0) {
+                // When we encounter a zero, we add the accumulated sum as a new node
                 curr.next = new ListNode(num);
                 curr = curr.next;
-                System.out.println(num);
-                num = 0;
-            }else{
+                num = 0; // Reset the sum after inserting the node
+            } else {
+                // Keep adding values between zeros
                 num += head.val;
             }
-            head = head.next;
+            head = head.next; // Move to the next node
         }
-        return dummy.next.next;
-        
+
+        return dummy.next; // Return the new list without the first dummy node
     }
 }
