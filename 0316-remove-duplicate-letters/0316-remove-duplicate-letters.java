f835f5749bd53e9cc28feee1f104cbc136ffdd4a
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-15 15:15:40 +0900

    Time: 4 ms (30.13%), Space: 42.6 MB (26.81%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -4,25 +4,26 @@ import java.util.Stack;
 class Solution {
     public String removeDuplicateLetters(String s) {
 
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
         Stack<Character> stack = new Stack();
         boolean[] inStack = new boolean[26]; // alphabet 
         
         for(char c : s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
+            freqMap.put(c,freqMap.getOrDefault(c,0)+1);
         }
         
         for(char c: s.toCharArray()){
-            map.put(c,map.get(c)-1);
+            freqMap.put(c,freqMap.get(c)-1);
 
             if(inStack[c-'a']){
                 continue;
             }
             
-            while(!stack.isEmpty() && stack.peek() >c && map.get(stack.peek()) > 0){
+            while(!stack.isEmpty() && stack.peek() > c && freqMap.get(stack.peek()) > 0){
                 inStack[stack.pop()-'a'] = false;
                 
             }          
+        
             stack.push(c);
             inStack[c-'a']=true;
         }
