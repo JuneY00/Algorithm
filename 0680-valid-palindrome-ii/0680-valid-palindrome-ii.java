f78d3a8b8c00131e6973974b3d077fb4af39b0e1
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-16 12:33:33 +0900

    Time: 4 ms (99.52%), Space: 45.5 MB (28.34%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,8 +1,6 @@
 class Solution {
     
-    private boolean isPalindrome(String s){
-        int left = 0;
-        int right = s.length()-1;
+    private boolean isPalindrome(String s, int left, int right){
         
         while(left<right){
             if(s.charAt(left) != s.charAt(right)){
@@ -20,23 +18,12 @@ class Solution {
         int right = s.length()-1;
         int count = 0;
         
-        
         while(left < right){
             
             if(s.charAt(left)!=s.charAt(right)){
-                String delLeft = s.substring(0,left)+s.substring(left+1,s.length());
-                String delRight = s.substring(0,right)+s.substring(right+1,s.length());
-            
-                System.out.println(delLeft);
-                System.out.println(delRight);
-                
-                if(isPalindrome(delLeft) || isPalindrome(delRight)){
-                    return true;
-                }else{
-                    return false;
-                }
+                return isPalindrome(s,left+1, right) || isPalindrome(s,left,right-1);
             }
-                   
+            
             left++;
             right--;
              
