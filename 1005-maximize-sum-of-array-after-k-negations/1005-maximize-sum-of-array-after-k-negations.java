889c55fb6564967d7ca35aae0cf4ae16b5705744
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-21 12:41:06 +0900

    Time: 4 ms (52.18%), Space: 42.4 MB (40.64%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.java
@@ -3,16 +3,12 @@ class Solution {
         Arrays.sort(nums);
         int result = 0;
             
-        // [-4, -3, 1, 2, 5 ]
-        
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] > -1){
-                result+=nums[i];
-            }else if(nums[i] < 0 && k > 0){
+            if(nums[i] < 0 && k > 0){ // nums[i] < 0 && k > 0 
                 nums[i] *=-1;    
                 result += nums[i];
                 k--; 
-            }else{ // nums[i] < 0 but, k < 0 
+            }else{ // (nums[i] < 0 but, k < 0)  or nums[i] > 0 
                 result += nums[i];
             }   
         }
@@ -20,7 +16,7 @@ class Solution {
         Arrays.sort(nums);
         
         if(k > 0 && k%2 == 1){
-            result += (nums[0]*-1)*2;
+            result += (nums[0]*-1)*2; // get the min value 
         }
     
         return result;
