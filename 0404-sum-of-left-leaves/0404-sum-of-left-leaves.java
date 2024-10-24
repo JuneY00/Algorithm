a67bfcc009306f8ac25aed205ca9c1f0661e6d58
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-24 12:23:36 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (37.47%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -13,69 +13,70 @@
  *     }
  * }
  */
-class Solution {
-    public int sumOfLeftLeaves(TreeNode root) {
 
+// class Solution {
+//     public int sumOfLeftLeaves(TreeNode root) {
+
+//         if(root == null){
+//             return 0;
+//         }
+    
+//         Stack<TreeNode> stack = new Stack<>();
+//         stack.push(root);
+//         int sum = 0;
+        
+//         while(!stack.isEmpty()){
+//             TreeNode node = stack.pop();
+            
+//             System.out.println(node.val);
+            
+//             // Check if left child exists
+//             if(node.left != null){
+                
+//                 // if the left child is a leaf node, and its value to the sum
+//                 if(node.left.left == null && node.left.right == null){
+//                     sum += node.left.val;
+//                 }else{
+//                     stack.push(node.left);
+//                 }
+//             }
+            
+//             //  if right child exists, push the right child to the stack 
+//             if(node.right != null){
+//                 stack.push(node.right);
+//             }
+            
+//         }
+        
+//         return sum;
+//     }
+// }
+
+
+
+class Solution {
+    private int treeRecursive(TreeNode root, boolean left){
         if(root == null){
             return 0;
         }
-    
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        int sum = 0;
         
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            
-            System.out.println(node.val);
-            
-            // Check if left child exists
-            if(node.left != null){
-                
-                // if the left child is a leaf node, and its value to the sum
-                if(node.left.left == null && node.left.right == null){
-                    sum += node.left.val;
-                }else{
-                    stack.push(node.left);
-                }
-            }
-            
-            //  if right child exists, push the right child to the stack 
-            if(node.right != null){
-                stack.push(node.right);
-            }
-            
+        if(root.left == null && root.right == null && left){
+            return root.val;
         }
         
-        return sum;
+        int leftSum = treeRecursive(root.left, true);
+        int rightSum = treeRecursive(root.right, false);
+        
+        return leftSum + rightSum; 
+        
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        if(root == null){
+            return 0;
+        }
+        
+        return treeRecursive(root, false);
+        
     }
 }
-
-
-
-// class Solution {
-//     private int treeRecursive(TreeNode root, boolean left){
-//         if(root == null){
-//             return 0;
-//         }
-        
-//         if(root.left == null && root.right == null && left){
-//             return root.val;
-//         }
-        
-//         int leftSum = treeRecursive(root.left, true);
-//         int rightSum = treeRecursive(root.right, false);
-        
-//         return leftSum + rightSum; 
-        
-//     }
-//     public int sumOfLeftLeaves(TreeNode root) {
-        
-//         if(root == null){
-//             return 0;
-//         }
-        
-//         return treeRecursive(root, false);
-        
-//     }
-// }
