e096cbf33beef27e7ff3763f0befb56bd81a778f
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-10-31 15:44:32 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (89.81%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -13,39 +13,71 @@
  *     }
  * }
  */
+
+
 class Solution {
+    private int first;
+    private int second=-1; 
+
       
     public int findSecondMinimumValue(TreeNode root) {
-        if(root==null || root.right == null || root.left == null){
+        if(root==null){
             return -1;
         }
-    
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        int first = root.val;
-        int second = -1;
-
-        while(!q.isEmpty()){
-            TreeNode node = q.poll(); 
-            
-            // Update 'second' only if the node value is greater than 'first'
-            // and either 'second' has not been updated yet(second==-1)
-            // or the node value is smaller than the current 'second'
-            if(node.val > first){
-                if(second == -1 || second > node.val){
-                    second = node.val;
-                }
-            }
-            
-            if(node.right != null && node.left != null){
-                q.add(node.left);
-                q.add(node.right);
-            }
-            
-        }
+        first = root.val;
+        traverse(root);
         
         return second;
+    }
+    
+    private void traverse(TreeNode node){
+        if(node==null){
+            return;
+        }
+        
+        if(node.val > first){
+            if(second == -1 || second > node.val){
+                second = node.val;
+            }
+        }
+        traverse(node.left);
+        traverse(node.right);
 
-     }
+    }
+    
 }
+// class Solution {
+      
+//     public int findSecondMinimumValue(TreeNode root) {
+//         if(root==null || root.right == null || root.left == null){
+//             return -1;
+//         }
+    
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+        
+//         int first = root.val;
+//         int second = -1;
+
+//         while(!q.isEmpty()){
+//             TreeNode node = q.poll(); 
+            
+//             // Update 'second' only if the node value is greater than 'first'
+//             // and either 'second' has not been updated yet(second==-1)
+//             // or the node value is smaller than the current 'second'
+//             if(node.val > first){
+//                 if(second == -1 || second > node.val){
+//                     second = node.val;
+//                 }
+//             }
+            
+//             if(node.right != null && node.left != null){
+//                 q.add(node.left);
+//                 q.add(node.right);
+//             }
+//         }
+        
+//         return second;
+
+//      }
+// }
