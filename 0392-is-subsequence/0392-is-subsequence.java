15c34feab5ad64e2dda0a8247f2343142488607d
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-11-11 14:35:51 +0900

    Time: 1 ms (93.04%), Space: 41.4 MB (40.83%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,41 +1,41 @@
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-       
-        int s_index=0 , t_index = 0;
-        
-        while(s_index < s.length() && t_index < t.length()){
-            if(s.charAt(s_index) == t.charAt(t_index)){
-                s_index++;
-            }
-            t_index++;
-        }
-        
-        return s_index == s.length();
-        
-    }
-}
-
 // class Solution {
 //     public boolean isSubsequence(String s, String t) {
-//         boolean[] dp = new boolean[s.length()];
-//         Arrays.fill(dp,false);
-//         int t_index = 0;
+       
+//         int s_index=0 , t_index = 0;
         
-//         for(int i = 0; i < s.length(); i++){
-            
-//             while(t_index < t.length()){
-//                 if(s.charAt(i) == t.charAt(t_index)){
-//                     dp[i] = true;
-//                     t_index++;
-//                     break;
-//                 }                
-//                 t_index++;
+//         while(s_index < s.length() && t_index < t.length()){
+//             if(s.charAt(s_index) == t.charAt(t_index)){
+//                 s_index++;
 //             }
-//             if(dp[i] == false){
-//                 return false;
-//             }
+//             t_index++;
 //         }
         
-//         return true;
+//         return s_index == s.length();
+        
 //     }
 // }
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        boolean[] dp = new boolean[s.length()];
+        Arrays.fill(dp,false);
+        int t_index = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            
+            while(t_index < t.length()){
+                if(s.charAt(i) == t.charAt(t_index)){
+                    dp[i] = true;
+                    t_index++;
+                    break;
+                }                
+                t_index++;
+            }
+            if(dp[i] == false){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
