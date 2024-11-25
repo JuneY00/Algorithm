a9a0b799384fbd98feeeee8abc82b50392838a8f
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-11-25 16:40:03 +0900

    Time: 11 ms (71.83%), Space: 57.8 MB (57.85%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        
+        for(int num: nums){
+            if(numSet.contains(num)){
+                return true;
+            }
+            numSet.add(num);
+        }
+        
+        return false;
+        
+        // for(int num: nums){
+        //     numSet.add(num);
+        // }
+        // return numSet.size() < num.length();
+    }
+}
