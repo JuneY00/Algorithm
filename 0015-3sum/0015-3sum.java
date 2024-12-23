1baff063091e7b8b8257ed46a89e8c20ea68e45b
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-12-23 17:43:48 +0900

    Time: 36 ms (31.65%), Space: 51.7 MB (45.35%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
     
-        // define set to remove duplicated lists
+        // Define a set to store unique triplets and avoid duplicates 
         Set<List<Integer>> result = new HashSet<>();
            
-        // to use binarysearch, we need to sort the array 
+        // Sort the array to simplify the two-pointer approach 
         Arrays.sort(nums);        
     
-        
+        // Iterate through the array, fixing one element (num[i]) at a time 
         for(int i = 0; i < nums.length-2; i++){
             
-            // skip duplicate number
+            // skip duplicate values for the fixed element 
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
             
+            // Initialize two pointers for the two-sum problems 
             int start = i+1;
             int end = nums.length-1; 
             
+            // Use two pointer approach to find pairs that sum to -num[i]
             while(start<end){
                 int sum = nums[i] + nums[start] + nums[end];
                 
@@ -26,14 +28,18 @@ class Solution {
                     start++;
                     end--;
                     
-                    // skip duplicate number 
+                    // Skip duplicate values for the start pointer 
                     while(start<end && nums[start] == nums[start-1]) start++;
+                    
+                    // Skip duplicate values for the end pointer
                     while(start<end && nums[end]==nums[end+1]) end--; 
                     
-                }else if(sum<0){ // if sum is less than zero, raise number by increasing start index 
+                }else if(sum<0){ 
+                    // if the sum is less than zero, move the start pointer to increase the sum
                     start++; 
                     
                 }else{
+                    // if the sum is less than zero, move the end pointer to decrease the sum
                     end--;
                 }
             }
