d04cbcbea4731aa65a0df3b456fb58fc7da3a6d5
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-12-26 00:31:47 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (46.76%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+    
+        
+        for(int i = 0; i<= high; i++){
+            
+            // if current number is 0
+            if(nums[i] == 0){
+                // swap with the number at the low index
+                swap(nums, low, i);
+                // move the low pointer forward 
+                low++;
+            }
+            // if the current number is 2
+            if(nums[i] == 2){
+                
+                // swap with the number at the high index
+                swap(nums, high, i);
+                
+                // move the high pointer backward  
+                high--;
+                
+                // decrease i to check the swapped value at index i 
+                
+                i--;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
