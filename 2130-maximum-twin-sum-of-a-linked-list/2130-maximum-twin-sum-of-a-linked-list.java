7b5dacfddbc80280a4b04546f76a442720a1c0f7
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-12-30 22:51:45 +0900

    Time: 4 ms (96.20%), Space: 63.4 MB (74.66%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -15,17 +15,18 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
-        // find the half of the head 
+        // find the middle of the Linked list 
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
         
-        // reverse the slow 
+        // Reverse the second half of the linked list starting from slow  
         ListNode curr = slow;
         ListNode prev = null;
         ListNode temp = null;
         
+        // Reverse the list using a loop 
         while(curr != null){
             temp = curr.next;
             curr.next = prev;
@@ -35,7 +36,9 @@ class Solution {
         
         int result = 0; 
         
+        // compare the nodes from the reversed second half with the first half
         while(prev != null ){
+            // calculate the sum of the current pair and update the maximum
             result = Math.max(result, prev.val + head.val);
             prev = prev.next;
             head = head.next;
