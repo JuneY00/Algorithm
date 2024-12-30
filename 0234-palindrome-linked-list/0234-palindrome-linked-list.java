4fc8d15dc45b9350667d448b01584fb9bc65eb98
JuneY00 <137476376+JuneY00@users.noreply.github.com>
2024-12-30 22:24:24 +0900

    Time: 164 ms (6.94%), Space: 65.2 MB (56.98%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -7,49 +7,50 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+//  */
+
+
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
+       
         if(head == null){
             return false;
         }
         
+        // find the middle of the linked list 
         ListNode slow = head;
         ListNode fast = head;
         
-        // 1. Find the middle of the linked list
-        while(fast !=null && fast.next != null){
+        // slow is the middle of the linked list 
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        
-        // 2. Reverse the second half of the linked list
-        ListNode prev = null;
-        ListNode next = null;
+
         ListNode curr = slow;
-        
+        ListNode temp = null;
+        ListNode prev = null;
+
+        // 2. Reverse the second half of the linked list
         while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+            temp = curr.next; 
+            curr.next = prev; 
+            prev = curr; 
+            curr = temp;             
         }
-            
         
-        ListNode left = head;
-        ListNode right = prev;
         
         // 3. Compare the two halves
-        while(right !=null){
-            if(left.val != right.val){
+        while(prev != null){
+            System.out.println(head.val + " "+ prev.val);
+            if(head.val != prev.val){
                 return false;
             }
-            
-            left = left.next;
-            right = right.next;
-
+            head = head.next;
+            prev = prev.next;
         }
-        
         return true;
+        
     }
 }
